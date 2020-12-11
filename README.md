89b0f4ea5f0a459fde75e4486aeac7e63f7d88d1
Tommy1856 <callum.hatfield@student.nua.ac.uk>
2020-12-11 16:09:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SneakTavernSim
+
+Developed with Unreal Engine 4
---
+++ Source/SneakTavernSim/SneakTavernSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SneakTavernSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNEAKTAVERNSIM_API ASneakTavernSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
